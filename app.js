7fd976a231d796364245f85bb76af68c1c3fc742
montikayoti <109443641+shinnjiwoong@users.noreply.github.com>
2022-12-07 18:27:38 +0900

    221207
    백엔드 처리과정 구현

--- app.js
+++ app.js
@@ -3,10 +3,16 @@ const express = require('express');
 const multer = require("multer");
 const tesseract = require("node-tesseract-ocr");
 const natural = require('natural');
+// const axios = require("axios");
+const cheerio = require("cheerio");
+const fetch = require("node-fetch");
+const parser = require("jsdom");
 const app = express()
 const port = 3000;
 
 var fileName;
+const stopwords = ['i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now', 'also'];
+const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -42,38 +48,60 @@ const config = {
 //   })
 
 
-var processedText = [];
+// var processedText = [];
 
 // Tokenization
 const tokens = async (img) => {
-  var word_array = []
+  const word_array = []
   await tesseract.recognize(img, config).then((sentence)=>{
     var tokens = tokenizer.tokenize(sentence);
     const len = Object.keys(tokens).length;
     var stem_tokens = [];
+    
+    // for(let i = 0; i<len; i++){
+    //   stem_tokens.push(natural.PorterStemmer.stem(tokens[i]));
+    //   stem_tokens[i] = stem_tokens[i].toLowerCase();
+    // }
     for(let i = 0; i<len; i++){
-      stem_tokens.push(natural.PorterStemmer.stem(tokens[i]));
-    }
-    for(let i = 0; i<len; i++){
-      if(stem_tokens[i] == 'a'){
-        continue;
-      }else if(stem_tokens[i] == 'of'){
-        continue;
-      }else if(stem_tokens[i] == 'This' || stem_tokens[i] == 'this'){
-        continue;
-      }else if(stem_tokens[i] == 'is'){
-        continue;
-      }else if(stem_tokens[i] == 'and'){
+      stem_tokens[i] = tokens[i].toLowerCase();
+      // console.log(stem_tokens[i])
+      if(stopwords.includes(stem_tokens[i])){
         continue;
       }
       word_array.push(stem_tokens[i]);
     };
   });
-  console.log('tokenization done!');
-  return word_array
+  const len = word_array.length; 
+  const crawled_word_meanings = await web_process(word_array);
+  let final_words = ''
+
+  // for(i=0; i<len; i++){
+  //   final_words = final_words + word_array[i] + '|MEANSPLIT|'
+  //   final_words = final_words + crawled_word_meanings[i] + '|WORDSPLIT|'
+  // }
+  console.log(final_words);
+  return [word_array, crawled_word_meanings]
 }
 
+const web_process = async (wordlist) => {
+  const resultArr = []
+  for(const word of wordlist){
+    const word_meaning = await request_crawl(word);
+    resultArr.push(word_meaning);
+  } 
+  //  console.log(resultArr); 
+  return resultArr
+}
+const request_crawl = async (word) => {
+  const response = await fetch(`https://dic.daum.net/search.do?q=${word}`)
+  const body = await response.text();
+  const $ = await cheerio.load(body);
+  // const title = $("#mArticle > div.search_cont > div:nth-child(3) > div:nth-child(2) > div > div.search_cleanword > strong > a > span").text();
+  const word_meaning = await $("#mArticle > div.search_cont > div:nth-child(3) > div:nth-child(2) > div > ul").text() + "|SPLIT|";
+  // console.log(word_meaning)
 
+  return word_meaning
+}
 app.get('/', (req, res, next) => {
   res.render('index');
   // console.log(processedText);
@@ -82,9 +110,11 @@ app.get('/', (req, res, next) => {
 app.post('/result', upload.single('image'), async (req, res, next) => {
   const img = `upload/${fileName}`;
   const final_words = await tokens(img);
-  const len = final_words.length;
+  const word_titles = final_words[0]
+  const meanings = final_words[1];
+  const len = word_titles.length;
   console.log(final_words);
-  res.render('result', {data: final_words, words_len: len});
+  res.render('result', {title: word_titles, words_len: len, meaning: meanings});
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
